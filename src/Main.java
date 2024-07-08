f0cf06867dae4dfadc98a2376b8f6f37e7ce38dc
fanweixu <fanwx1204@gmail.com>
2024-07-09 01:04:39 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
+// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+import Ui.gamejframe;
+import Ui.loginjframe;
+import Ui.registerjframe;
+public class Main {
+    public static void main(String[] args) {
+        gamejframe game=new gamejframe();
+        loginjframe login=new loginjframe();
+        registerjframe register=new registerjframe();
+
+    }
+
+
+}
